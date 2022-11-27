d4ce238e3b1bf1f3109e02db2cbf5db58a3acb1c
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-28 03:03:12 +0800

    Save data support almost complete

--- Game.Restore.cs
+++ Game.Restore.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 
 namespace libblitz;
@@ -12,22 +11,74 @@ public partial class Game
 		public Guid Uid;
 		public string DisplayName;
 
-		public IList<ActivityDescription> ActivityStack { get; set; }
-		public ActivityResult Result;
+		public IList<GameMember.SaveData> Members { get; set; }
+		public IList<ActivityDescription.SaveData> ActivityStack { get; set; }
+		public string ActivityResultTypeName;
+		public string ActivityResultSerialized;
 
 		public string Serialize() => JsonSerializer.Serialize( this );
 		public static SaveData From( string data ) => JsonSerializer.Deserialize<SaveData>( data );
 	}
 
-	public Game( SaveData saveData )
-	{
-	}
+	public Game( SaveData saveData ) => LoadSaveData( saveData );
 
 	private void LoadSaveData( SaveData saveData )
 	{
 		Uid = saveData.Uid;
 		DisplayName = saveData.DisplayName;
 
-		ActivityStack = ActivityStack.ToList();
+		ActivityStack.Clear();
+
+		// Populate activity stack
+		foreach ( var actStackSave in saveData.ActivityStack )
+		{
+			ActivityStack.Add( new ActivityDescription( actStackSave ) );
+		}
+
+		// Attempt to copy over result
+		if ( saveData.ActivityResultTypeName != null )
+		{
+			var typeDesc = TypeLibrary.GetDescription( saveData.ActivityResultTypeName );
+			_activityResult = TypeLibrary.Create<ActivityResult>( saveData.ActivityResultTypeName );
+			NonGenericJson.DeserializeTo( typeDesc.TargetType, saveData.ActivityResultSerialized, typeDesc.TargetType,
+				_activityResult );
+		}
+
+		// Jump to creating new activity
+		_shouldInitActivityInstance = true;
+		AttemptActivityInit();
+	}
+
+	public SaveData ToSaveData()
+	{
+		var instance = new SaveData
+		{
+			Uid = Uid,
+			DisplayName = DisplayName,
+			ActivityStack = new List<ActivityDescription.SaveData>(),
+			Members = new List<GameMember.SaveData>()
+		};
+
+		// set up ActivityStack
+		foreach ( var description in ActivityStack )
+		{
+			instance.ActivityStack.Add( description.ToSaveData() );
+		}
+
+		// set up ActivityResult*
+		if ( _activityResult != null )
+		{
+			var typeDesc = TypeLibrary.GetDescription( _activityResult.GetType() );
+			instance.ActivityResultTypeName = typeDesc.TargetType.FullName;
+			instance.ActivityResultSerialized = NonGenericJson.Serialize( typeDesc.TargetType, _activityResult );
+		}
+
+		// set up Members
+		foreach ( var member in Members )
+		{
+			instance.Members.Add( member.ToSaveData() );
+		}
+
+		return instance;
 	}
 }
