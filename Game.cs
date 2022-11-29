4a87658792eac1f1a857a16aeca50c293ad44d0c
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-30 01:46:48 +0800

    Save turn player UID

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using Sandbox;
 using Sandbox.UI;
@@ -14,14 +15,20 @@ public abstract partial class Game : Sandbox.Game
 	[Net] public string DisplayName { get; set; }
 	[Net] public IList<GameMember> Members { get; set; } = new List<GameMember>();
 	[Net] public int Turns { get; set; }
-	private Guid LastTurnPlayer { get; set; }
+	private Guid LastTurnPlayerUid { get; set; } = Guid.Empty;
+
+	public GameMember LastTurnPlayer
+	{
+		get => Members.SingleOrDefault( v => v.Uid == LastTurnPlayerUid );
+		set => LastTurnPlayerUid = value.Uid;
+	}
 
 	public class SaveData : ISaveData
 	{
 		public Guid Uid { get; set; }
 		public string DisplayName { get; set; }
 		public int Turns { get; set; }
-		public Guid LastTurnPlayer { get; set; }
+		public Guid LastTurnPlayerUid { get; set; }
 		public IList<GameMember.SaveData> Members { get; set; }
 		public IList<ActivityDescription.SaveData> ActivityStack { get; set; }
 		public string ActivityResultTypeName { get; set; }
